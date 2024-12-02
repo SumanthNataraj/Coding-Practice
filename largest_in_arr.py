f4bffe5028ce29ce289066ff02674af5cfe3d01c
SumanthNataraj <sumanthsuman1975@gmail.com>
2024-12-02 19:47:54 +0530

    largest in array

---
+++ largest_in_arr.py
@@ -0,0 +1,44 @@
+"""Largest Element in Array
+
+Write a function to find the largest element in an array.
+Given an array arr[]. The task is to find the largest element and return it."""
+
+from typing import List
+
+
+class Solution:
+    def largest(self, arr : List[int]) -> int:
+        # code here
+        arr=sorted(arr)
+        
+        return arr[-1]
+        
+
+
+
+class IntArray:
+
+    def __init__(self) -> None:
+        pass
+
+    def Input(self, n):
+        arr = [int(i) for i in input().strip().split()]  #array input
+        return arr
+
+    def Print(self, arr):
+        for i in arr:
+            print(i, end=" ")
+        print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        arr = list(map(int, input().split()))
+
+        obj = Solution()
+        res = obj.largest(arr)
+
+        print(res)
+        print("~")
