4af180289e00679b669f3cddac2148938bcf79af
SumanthNataraj <sumanthsuman1975@gmail.com>
2024-12-24 19:32:16 +0530

    Min_and_Max_in_Array

---
+++ Min_and_Max_in_Array.py
@@ -0,0 +1,16 @@
+"""Given an array arr. Your task is to find the minimum and maximum elements in the array.
+
+Note: Return an array that contains two elements the first one will be a minimum element and the second will be a maximum of an array."""
+def get_min_max(self, arr):
+        
+        a=min(arr)
+        b=max(arr)
+        return a, b
+    
+# Test the function
+
+arr = [1, 2, 3, 4, 5]
+
+print(get_min_max(arr)) # Output: (1, 5)
+
+arr = [-10, -20, -30, -40, -50]
